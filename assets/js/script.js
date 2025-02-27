0d594eb35a3e01323bb2b863cbaf0eeaab0dfed1
Joseph Pal <josephpal27@gmail.com>
2025-02-27 17:08:04 +0530

    add banner slider functionality and new section for "What We Do"

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+
+// Banner Slider Functionality
+var swiper = new Swiper('.swiper-container', {
+    effect: 'fade',
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    fadeEffect: {
+        crossFade: true,
+    },
+    grabCursor: true,
+});
+
+// --------------------------------------------------------------------------------------------------------------
