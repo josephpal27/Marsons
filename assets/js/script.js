7fc3023210cd9e617ef4f2960318fe18889711b8
Joseph Pal <josephpal27@gmail.com>
2025-03-04 18:19:50 +0530

    add back-to-top button functionality and contact page styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,25 @@ footIconBox.forEach((box, i) => {
 })
 
 // --------------------------------------------------------------------------------------------------------------
+
+// Functionality For Back to Top Button
+document.addEventListener('DOMContentLoaded', function() {
+    const lenis = new Lenis({
+        duration: 0.5, // Adjust the duration for smooth scrolling
+        easing: (t) => t * (2 - t),
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    // Back to Top button functionality
+    const backToTopButton = document.querySelector('.back-to-top');
+    backToTopButton.addEventListener('click', function() {
+        lenis.scrollTo(0, { duration: 0.5 });
+    });
+});
+
+// --------------------------------------------------------------------------------------------------------------
